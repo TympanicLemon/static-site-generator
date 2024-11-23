cfcf54ccd84a109cfcc36afbfe975cd4d33a6a2f
Daniel Zapata <zdaniel7697@gmail.com>
2024-11-23 13:13:34 -0800

    fixed __repr__ for HTMLNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -15,9 +15,12 @@ class HTMLNode:
         return "No props are present in this html node"
 
     def __repr__(self):
-        return f"""HTMLNode:
-        tag={self.tag}
-        value={self.value}
-        children={self.children}
-        props={self.props}
-    """
+        return (
+            f"HTMLNode:\n"
+            f"tag={self.tag}\n"
+            f"value={self.value}\n"
+            f"children={self.children}\n"
+            f"props={self.props}"
+        )
+
+
